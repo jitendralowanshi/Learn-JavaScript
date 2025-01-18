a13cd21995e201003995414fca630abee9a3812a
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2025-01-18 21:56:58 +0530

    learn strings

--- _03_loops_&Strings/_05_string_method.js
+++ _03_loops_&Strings/_05_string_method.js
@@ -1,23 +1,31 @@
 // These are built in function to manipulate a string 
-/**
+/*
  * strings are immutable in js 
+ 
     str.toUpperCase()
     str.toLowerCase()
-    str.trim()  // remove all white spaces
- */
+    str.trim()                  // remove all white spaces
+    str.slice(start, end)       //return part of String
+    
+    str1.concat(str2)           //joins atr2 with str1
+    str.replace(searchVal, newVal)
+    str.charAt(idx)
+
+*/
 
 let str = "JiTEndra";
 let str2 = "lowanshi";
-let str3 = "    jitendraLowanshi      ";
+let str3 = "    jitendraLowanshi    "; 
+
 
 
 console.log(str.toLowerCase());
 console.log(str2.toUpperCase());
 
+
 // console.log(str3.trimStart());
 console.log(str3.trimEnd());
 console.log(str3.trim());
-
 
 // method return new string --> does not change original string in js 
 
@@ -29,3 +37,7 @@ console.log(app);
 
 // original string does not change 
 console.log(newStr);
+
+
+// replace 
+// replcaAll
