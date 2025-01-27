abfea74a93604143d82b17523a17ec36f6e99cbd
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2025-01-27 20:21:43 +0530

    add dom file

---
+++ Lec_06_Dom/_01_windowObject.js
@@ -0,0 +1,7 @@
+/**
+ The window object represent an open window in a brouser .
+ It us brouwser's object (not javaScripts's) & is automatically created by brouser.
+
+ It is a global object with lots of propreties & methods
+ 
+ */
