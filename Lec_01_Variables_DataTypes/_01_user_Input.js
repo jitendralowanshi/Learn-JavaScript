3ced6b5f2c949eb820a978c0f4f4455211b0cf03
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2025-01-30 09:45:21 +0530

    modified files

--- Lec_01_Variables_DataTypes/_01_user_Input.js
+++ Lec_01_Variables_DataTypes/_01_user_Input.js
@@ -1,3 +1,20 @@
 let name = prompt("enter your name :");
 
 let alt = alert("Hello!");
+
+/*
+// 8 DATA TYPES IN JAVASCRIPT 
+primitive data types 
+
+1> Number
+2> String
+3> Boolean
+4> Undefined
+5> Null
+6> bigint
+7> Symbol
+
+Non primitive data types 
+8> object
+
+*/
